e1e8e071266ff3e37610231fdde8c5ae1bf9004f
Liana <evangelia.alevrakis@gmail.com>
2025-03-02 12:38:52 +0100

    second commit

---
+++ 01-Intro/1.1.js
@@ -0,0 +1,1 @@
+//Using Visual Studio to test
---
+++ 01-Intro/1.2.js
@@ -0,0 +1,8 @@
+let name = "Evangelia"
+let firstName = "Alevrakis"
+let city = "Namur"
+console.log('My name is ' + name + ' ' + firstName + ' and I live in ' + city)
+
+
+
+
